837d4b82f9dddf8bd1ba45c6c377b1efc5f5c0d0
Chienlin1014 <shzerokimo@gmail.com>
2022-08-24 23:32:40 +0800

    modified

--- javascript-exercise-07/ch05/Hw04.js
+++ javascript-exercise-07/ch05/Hw04.js
@@ -1,9 +1,28 @@
 // 4. 有一字串陣列內容如下..
 // const mySkills = ['javascript', 'java', 'sql', 'c', 'python', 'php', 'swift'];
-let count = [
-    ['a', 0], ['c', 0], ['f', 0], ['h', 0], ['i', 0],
-    ['j', 0], ['l', 0], ['n', 0], ['o', 0], ['p', 0],
-    ['q', 0],['r',0],['s',0],['t',0],['v',0],['w',0],
-    ['y',0]
-
-];
+// 請統計並印出陣列內每個字母各有幾個，EX..
+// a: 4
+// c: 2
+// ..
+const mySkills = ['javascript', 'java', 'sql', 'c', 'python', 'php', 'swift'];
+let str=[];
+for (let i = 0; i < mySkills.length; i++) {
+    for (str[str.length] of mySkills[i]) {
+    }
+}
+str=str.sort();
+let alphabet=[];
+let letter=[];
+for (let i = 0; i <str.length ; ) {
+    letter=[];
+    for (let j = 0; j <str.length ; j++) {
+        if (str[i] === str[j]) {
+            letter[letter.length] = str[i];
+        }
+    }
+    alphabet[alphabet.length]=letter;
+    i+=letter.length;
+}
+for (let i = 0; i < alphabet.length; i++) {
+    console.log('字母' + alphabet[i][0] + '，共有' + alphabet[i].length + '個');
+}
