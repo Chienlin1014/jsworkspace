58c9937bd9a4844c3358dd548cc3ed55929dad84
Chienlin1014 <shzerokimo@gmail.com>
2022-08-25 23:00:02 +0800

    modified

--- javascript-exercise-07/ch06/Hw08.js
+++ javascript-exercise-07/ch06/Hw08.js
@@ -5,25 +5,17 @@
 // a: 底數
 // 回傳: 平方值
 //     :bulb: 提示: 請利用函式power來完成power2的功能
-
-/**
- *
- * @param {number} a 底數
- * @param {number} b 次方數
- * @returns {number}
- */
 function power(a, b) {
     return a**b;
 }
-
 /**
  *
  * @param {number} a 底數
  * @param {function} power
  * @returns {*} 回傳power的回傳值
  */
-function power2(a, power) {
+function power2(a) {
     return power(a,2);
 }
 
-console.log(power2(10, power));
+console.log(power2(10));
